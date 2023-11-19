d4138c5fe8a7a3540759d200be7932f0de7655a0
Locquaino <Locquianopatrickgreg@gmail.com>
2023-11-19 21:34:27 +0800

    Unfinished Project

--- app/src/main/java/com/example/reviewercompanion/activityQuizCategory.java
+++ app/src/main/java/com/example/reviewercompanion/activityQuizCategory.java
@@ -1,18 +1,35 @@
 package com.example.reviewercompanion;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.reviewercompanion.R.id;
+import static com.example.reviewercompanion.R.layout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class activityQuizCategory extends AppCompatActivity {
-    public static String category;
     public static int total_question_num;
+    static String category;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz_category);
-
+        setContentView(layout.activity_quiz_category);
     }
 
-
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item is selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos).
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback.
+    }
 }
